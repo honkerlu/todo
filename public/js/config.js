45486d44b103e7d696b1298d3656f943ab1384bf
honker110 <honker.lu@gmail.com>
2016-01-28 16:59:26 +0800

    to todo1

--- public/js/config.js
+++ public/js/config.js
@@ -1,5 +1,5 @@
 /**
  * Created by lzw on 14-8-21.
  */
-var appId='7Vqk3RX2rfvg9sJk7oQVWPR8-gzGzoHsz';
-var appKey='l2OUrS5z2knuFw3zxdFcxJiP';
+var appId='kcAFtpsVfr1Uk4NBoUGziXr6-gzGzoHsz';
+var appKey='zyjymMhLcvrjNLab4xxeVDAU';
